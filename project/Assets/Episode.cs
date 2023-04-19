41bb10cd4082dba5a026521cd8cd89fe988864ca
AnneKir <57183946+AnneKir@users.noreply.github.com>
2023-04-19 16:41:22 +0200

    flagheights

--- project/Assets/Episode.cs
+++ project/Assets/Episode.cs
@@ -18,8 +18,16 @@ public class Episode
     public bool featureLength;
     public string stardateFrom;
     public string stardateTo;
-    public string yearFrom;
+    public int yearFrom;
     public string yearTo;
     public string usAirDate;
     public string finalScriptDate;
 }
+
+class EpisodeComparer: IComparer
+{
+    public int Compare(object x, object y)
+    {
+        return (((Episode)x).yearFrom - ((Episode)y).yearFrom);
+    }
+} 
